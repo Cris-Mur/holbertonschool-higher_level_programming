9e44f97fd00fdb51222adacf018fbd05a00d76e2
Cris-Mur <yomy98@gmail.com>
2020-05-07 20:39:21 -0500

    [pycodestyle]

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if isinstance(roman_string, str) == False or roman_string is None:
+    if isinstance(roman_string, str) is False or roman_string is None:
         return 0
 
-    rever=''.join(reversed(roman_string))
-    roman={'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    rever = ''.join(reversed(roman_string))
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     sum = 0
 
     for x in rever:
