f56981d4a869d1bd8a2eb442300ae9506aac0f98
Cris-Mur <yomy98@gmail.com>
2020-05-18 16:56:17 -0500

    [add task 0]

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    w = 0
+    for i in range(x):
+        try:
+            print(my_list[i], end="")
+            w += 1
+        except:
+            break
+    print()
+    return w
