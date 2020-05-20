ef4e9612efae0bc2ec0e488a2280711e02ff96e2
Cris-Mur <yomy98@gmail.com>
2020-05-19 20:09:07 -0500

    [unnecessary lines]--

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Square class """
+
+
+class Square:
+    """ Define square size """
+    def __init__(self, new_size=0):
+        if type(new_size) == int:
+            if new_size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = new_size
+        else:
+            raise TypeError("size must be an integer")
+
+    """ Define square area """
+    def area(self):
+        return self.__size**2
