e484c5e0631a8e0477168ebe4caacf1a0dd88201
Cris-Mur <yomy98@gmail.com>
2020-05-19 20:44:04 -0500

    [add position properties]

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -5,20 +5,8 @@
 class Square:
     """ Define square init with pos"""
     def __init__(self, new_size=0, new_position=(0, 0)):
-        if type(new_size) == int:
-            if new_size < 0:
-                raise ValueError("size must be >= 0")
-            else:
-                self.__size = new_size
-        else:
-            raise TypeError("size must be an integer")
-
-        if (type(new_position[0]) == int and type(new_position[1]) == int or
-            type(new_position) == tuple and len(new_position) == 2 and
-                new_position[0] > 0 and new_position[1] > 0):
-            self.__position = new_position
-        else:
-            raise TypeError("position must be a tuple or 2 positive integers")
+        self.__size = new_size
+        self.__position = new_position
 
     """ Define square area """
     def area(self):
@@ -53,3 +41,18 @@ class Square:
 
         if self.__size == 0:
             print()
+
+    """ Define property position """
+    @property
+    def position(self):
+        return self.__position
+
+    """ Define setter position """
+    @position.setter
+    def position(self, value):
+        if (type(new_position[0]) == int and type(new_position[1]) == int or
+            type(new_position) == tuple and len(new_position) == 2 and
+                new_position[0] > 0 and new_position[1] > 0):
+            self.__position = new_position
+        else:
+            raise TypeError("position must be a tuple or 2 positive integers")
