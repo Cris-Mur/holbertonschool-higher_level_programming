384f5038913b5d21d29df52891196ed64cf30178
Cris-Mur <yomy98@gmail.com>
2020-05-22 23:15:47 -0500

    [6 test]

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -6,36 +6,36 @@ max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
     """ test papu """
-    def (self):
+    def test_normal(self):
         coso = [1, 2, 3, 4]
-        self.asserEqual(coso, 4)
+        self.assertEqual(max_integer(coso), 4)
 
-    def (self):
+    def test_negative(self):
         coso = [-4, -3, -2, -1]
-        self.asserEqual(coso, -1)
+        self.assertEqual(max_integer(coso), -1)
 
-    def (self):
+    def test_alone(self):
         coso = [42]
-        self.asserEqual(coso, 42)
+        self.assertEqual(max_integer(coso), 42)
 
-    def (self):
+    def test_none(self):
         coso = []
-        self.asserEqual(coso, None)
+        self.assertEqual(max_integer(coso), None)
 
-    def (self):
+    def test_float(self):
         coso = [1.2, 2.3, 4.5, 5.6]
-        self.asserEqual(coso, 5.6)
+        self.assertEqual(max_integer(coso), 5.6)
 
-    def (self):
+    def test_reppit(self):
         coso = [1, 1, 2, 9, 3, 3, 5, 5, 9]
-        self.asserEqual(coso, 9)
+        self.assertEqual(max_integer(coso), 9)
 
-    def (self):
-        with self.asserRaises(TypeError):
+    def test_tERR_l(self):
+        with self.assertRaises(TypeError):
             max_integer([1, 3, 'f'])
 
-    def (self):
-        with self.asserRaises(TypeError):
+    def test_tERR_s(self):
+        with self.assertRaises(TypeError):
             max_integer({6, 7})
 
 if __name__ == '__main__':
