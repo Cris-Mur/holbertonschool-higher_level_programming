f4f441d835ca3c50e4a512b338254da8b2e36c38
Cris-Mur <yomy98@gmail.com>
2020-06-03 21:49:57 -0500

    [comentarios]

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,12 @@
 #!/urs/bin/python3
-""" function that open a text file and print this is stdout """
+"""
+function that open a text file and print this is stdout
+"""
 
 
 def read_file(filename=""):
-    """ read file function """
+    """
+    read file function
+    """
     with open(filename, mode='r', encoding="utf-8") as coso:
         print(coso.read(), end='')
