f4e88815ea886dd725b4fa6f26ba4b8952307c25
Cris-Mur <yomy98@gmail.com>
2020-06-03 23:32:07 -0500

    [volando]

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+''' class to json '''
+
+
+class Student:
+    ''' class student '''
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    ''' return dict '''
+    def to_json(self, attrs=None):
+        if type(attrs) != list:
+            return self.__dict__
+        else:
+            return dict(filter(lambda i: i[0] in attrs, self.__dict__.items()))
+
+    def reload_from_json(self, json):
+        for key, valur in json.items():
+            if hasattr(self, key):
+                setattr(self, key, valur)
