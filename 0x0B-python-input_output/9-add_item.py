3a47679d70406c565cea7eb8f8c1e08ae17af8ad
Cris-Mur <yomy98@gmail.com>
2020-06-03 23:05:21 -0500

    [sad x3]

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -3,22 +3,14 @@
 load, add, save
 """
 
-
 import sys
-import json
 
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
-if __name__ == "__main__":
 
 try:
-    coso_json = load_from_json_file("add_item.json")
+    coso_json = load_from_json_file("add_item.json") + sys.argv[1:]
 except:
-    coso_json = []
-
-argc = len(sys.argv)
-for ant in range(1, argc):
-    coso_json.append(sys.argv[i])
-
+    coso_json = sys.argv[1:]
 save_to_json_file(coso_json, "add_item.json")
