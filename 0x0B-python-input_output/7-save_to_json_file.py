2bebf822f9249b7049ba238f58ab22362a1df7c2
Cris-Mur <yomy98@gmail.com>
2020-06-03 22:03:02 -0500

    [seran los comentarios]

--- 0x0B-python-input_output/7-save_to_json_file.py
+++ 0x0B-python-input_output/7-save_to_json_file.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-"""  """
+'''
+saves a json file
+'''
 
 
 def save_to_json_file(my_obj, filename):
-    """  """
+    '''
+    function that save json file
+    '''
     import json
     with open(filename, mode='w', encoding="utf-8") as coso:
         json.dump(my_obj, coso)
