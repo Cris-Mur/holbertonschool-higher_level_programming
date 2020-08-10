b16f9d0c5695e16903a898b582f88d31aaf861b1
Cris-Mur <yomy98@gmail.com>
2020-08-09 23:45:13 -0500

    [task 8]

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+''' Script that list Data in a SQLdb '''
+import sys
+from sqlalchemy import (create_engine)
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+
+    user=sys.argv[1]
+    passwd=sys.argv[2]
+    db=sys.argv[3]
+
+    datB = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(user,
+                                                         passwd,
+                                                         db),
+        pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    query = session.query(State).filter(State.id)
+    table = query.first()
+    if not table:
+        print("{}".format("Nothing"))
+    else:
+        print("{:d}: {:s}".format(table.id, table.name))
+
+    session.close()
