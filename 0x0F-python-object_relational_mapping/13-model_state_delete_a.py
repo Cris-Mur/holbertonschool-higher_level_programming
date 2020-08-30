04701a303ff0e15088050f639e8c4be07d6b996f
Cris-Mur <yomy98@gmail.com>
2020-08-30 21:58:48 +0000

    [create script in base 12]

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+''' Script that list Data in a SQLdb '''
+import sys
+from sqlalchemy import (create_engine)
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    db = sys.argv[3]
+
+    datB = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(user,
+                                                         passwd,
+                                                         db),
+        pool_pre_ping=True)
+
+    Session = sessionmaker(bind=datB)
+    session = Session()
+
+    for state in session.query(State).filter(State.name.contains('a')).all():
+        session.delete(state)
+    session.commit()
+    session.close()
