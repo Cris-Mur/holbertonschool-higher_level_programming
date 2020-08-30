20506d5dae74df73831cdc3570a427519b0a7f4e
Cris-Mur <yomy98@gmail.com>
2020-08-30 02:07:40 +0000

    [fix engine]

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,23 +1,19 @@
 #!/usr/bin/python3
 ''' Script that list Data in a SQLdb '''
 import sys
-from sqlalchemy import (create_engine)
-from model_state import Base, State
+from sqlalchemy import create_engine
+from model_state import State
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == '__main__':
-
     user = sys.argv[1]
     passwd = sys.argv[2]
     db = sys.argv[3]
 
     datB = create_engine(
-        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(user,
-                                                         passwd,
-                                                         db),
-        pool_pre_ping=True)
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(user, passwd, db))
 
-    Session = sessionmaker(bind=engine)
+    Session = sessionmaker(bind=datB)
     session = Session()
 
     for point in session.query(State).order_by(State.id):
