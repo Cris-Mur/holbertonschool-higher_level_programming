72a777ca7e42117844bd4974cbab46d7df676241
Cris-Mur <yomy98@gmail.com>
2020-09-02 15:40:37 -0500

    [style]

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Python script that takes your Github credentials (username and password) """
+""" Python script that takes your Github credentials (username & password) """
 from requests import get, auth
 import sys
 if __name__ == '__main__':
