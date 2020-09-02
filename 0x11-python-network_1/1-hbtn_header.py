8a515ad24d37b4a26c126feb6c02ad01e738a124
Cris-Mur <yomy98@gmail.com>
2020-09-02 15:28:16 -0500

    [eteee]

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """ Python script that takes in a URL, sends a request to the URL """
-from urllib import request
-if __name__ == '__main__':
-    with request.urlopen('https://intranet.hbtn.io/status') as response:
-        print(response.getheader('X-Request-Id'))
+if __name__ == "__main__":
+    import urllib.request as request
+    from sys import argv
+    url = request.Request(argv[1])
+    with request.urlopen(url) as res:
+        print(res.headers.get('X-Request-Id'))
