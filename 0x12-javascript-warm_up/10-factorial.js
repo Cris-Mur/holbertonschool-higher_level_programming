15c314281a5fff43edcd5930906f02433da097fb
Cris-Mur <yomy98@gmail.com>
2020-09-16 18:29:52 -0500

    [10 to 13]

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+
+const n = parseInt(process.argv[2]);
+
+const facto = (n) => {
+  if (n > 1) {
+    return (facto(n - 1) * n);
+  } else {
+    return (n < 0 ? -1 : 1);
+  }
+};
+
+console.log(facto(n));
--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+
+if (process.argv.length <= 3) {
+  console.log(0);
+} else {
+  let stp;
+  let bucket = [];
+  for (stp = 2; stp < process.argv.length; stp++) {
+    bucket.push(parseInt(process.argv[stp]));
+  }
+  bucket = bucket.sort(function (x, y) {
+    return x - y;
+  });
+  console.log(bucket[bucket.length - 2]);
+}
