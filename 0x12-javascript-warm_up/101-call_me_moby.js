92b6331d99544edfa9fcc2f8b41d71f870e60a96
Cris-Mur <yomy98@gmail.com>
2020-09-16 20:50:11 -0500

    [add advanced task]

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+
+exports.callMeMoby = function (x, theFunction) {
+  let stp = 0;
+  for (stp = 0; stp < x; stp++) {
+    theFunction();
+  }
+}
--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,1 +1,5 @@
 #!/usr/bin/node
+exports.addMeMaybe = function (number, theFunction) {
+  const plus = number + 1;
+  theFunction(plus);
+};
