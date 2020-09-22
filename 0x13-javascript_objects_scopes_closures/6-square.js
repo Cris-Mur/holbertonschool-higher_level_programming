ff3258d9fca7812c00a221e7fbfa2bfeba8d63a3
Cris-Mur <yomy98@gmail.com>
2020-09-21 20:30:53 -0500

    [print char]

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (cosito) {
+		if (!cosito) {
+			super.print();
+		} else {
+			let y;
+			for (y = 0; y < this.height; y++) {
+				console.log(cosito.repeat(this.width));
+			}
+		}
+	}
+}
+
+module.exports = Square;
