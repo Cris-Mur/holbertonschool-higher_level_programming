0355b03a54e30d2c191a6318df391b01e491ceb3
Cris-Mur <yomy98@gmail.com>
2020-09-21 21:19:09 -0500

    [uff el semistandar.... how to standarize this... :C]

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
 
 exports.nbOccurences = function (list, searchElement) {
-		let occ = 0, x;
-		for (x = 0; x < list.length; x++) {
-			if (list[x] === searchElement) {
-				occ++;
-			}
-		}
-		return occ;
+  let occ = 0;
+  let x;
+  for (x = 0; x < list.length; x++) {
+    if (list[x] === searchElement) {
+      occ++;
+    }
+  }
+  return occ;
 };
