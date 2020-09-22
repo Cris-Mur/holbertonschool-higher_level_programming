b9fda365f8794f0d6dbdd837d68261da7318212a
Cris-Mur <yomy98@gmail.com>
2020-09-21 21:32:39 -0500

    [reverse]

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+exports.esrever = function (list) {
+  let x = list.length - 1;
+  let NL = [];
+  for (let i = 0; x >= 0; x--, i++) {
+    NL[i] = list[x];
+  }
+  return NL;
+}
